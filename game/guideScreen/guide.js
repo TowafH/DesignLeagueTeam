01837abd6ded3f2d8b3e9c95216e57487f259e87
Towaf H <144939977+TowafH@users.noreply.github.com>
2024-05-09 17:01:20 +0000

    update

--- game/guideScreen/guide.js
+++ game/guideScreen/guide.js
@@ -2,3 +2,11 @@ function moveToInstructions2(){
   window.location.href = "guide2.html";
 }
 
+function moveToInstructions3(){
+  window.location.href = "guide3.html";
+}
+
+function moveToInstructions4(){
+  window.location.href = "guide4.html";
+}
+
